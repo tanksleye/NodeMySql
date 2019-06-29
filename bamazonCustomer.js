88be6cc92692d70c73e99ddae41c45bb41c4b47e
Jet <elesebatanksley@yahoo.com>
2019-06-28 23:21:18 -0500

    added packages, fixed prompt syntax and made query connections to go through table to retreve user input

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,39 +1,96 @@
 var inquirer = require("inquirer");
+var mysql = require("mysql");
 
-function Purchase(id, units) {
-    this.id = id;
-    this.units = units;
+var connection = mysql.createConnection({
+    host: "localhost",
 
-    this.makePurchase = function() {
-        if (Math.floor(Math.random() * 2) === 0) {
-            this.units--;
-            console.log("There are " + units + " units left");
-        }
-    };
-    console.log("Product ID is: " + this.id);
-    console.log("How many units are left: " + this.units);
-}
+    PORT: 3306,
+
+    user: "root",
+    password: "35l4eJazz",
+    database: "bamazone_db"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    promptPurchase();
+  });
+
+//   app.listen(PORT, function() {
+//     // Log (server-side) when our server has started
+//     console.log("Server listening on: http://localhost:" + PORT);
+//   });
+  
+var table = "SELECT * FROM products";
+connection.query(table, function (err, res) {
+    console.log(res);
+});
+
 
 function promptPurchase() {
     console.log("\n--------\npromptPurchase: Select your purchase\n--------\n");
     inquirer.prompt([
-        {name: "ID",
-        message: "What is the product ID that you would like to buy: "
-    }, {
+        {
+        name: "productID",
+        // type: "rawlist",
+        // message: "What can I help you with?: ",
+        // choices: [
+            message: "What item ID are you looking for",
+        },{
         name: "units",
-        message: "How many units of the porduct would you like to buy",
-        validate: function(value) {
-            if (isNaN(valve) === false && parseInt(value) > 0 && parseInt(value) <=10){
-                return true;
-            }
-            return false;
+            message: "How many units would you like",
         }
-    }
+        // validate: function(value) {
+        //     if (isNaN(valve) === false && parseInt(value) > 0 && parseInt(value) <=10){
+        //         return true;
+        //     }
+        //     return false;
+        // }
     ])
     .then(function(answer) {
-        var purchase = new Purchase
-    })
-};
+        // switch (answer.action) {
+        //     case "What product ID are you looking for":
+        //         findProduct();
+        //         break;
+
+        //     case "How many units would you like":
+        //         amoundOfProduct();
+        //         break;
+        // }
+        var productID = answer.productID;
+        var units = answer.units;
+        var query = "SELECT * FROM products WHERE ?";
+
+        connection.query(query, { item_id: answer.productID }, function (err,res) {
+            console.log(res);
+        
+        var currentQuantity = res[0].stock_quantity;
+        var newQuantity = currentQuantity - units;
+        console.log("There are " + newQuantity + " left");
+
+        var currentPrice = res[0].price;
+        var totalCost = (units * currentPrice);
+            connection.query(totalCost, function (err, res) {
+                console.log("your total cost is " + totalCost);
+ 
+        }); 
+
+        var updateQuery = "UPDATE products SET stock_quantity = ? WHERE item_id = ?";
+        connection.query(updateQuery, [newQuantity, productID], function (err, res) {
+            console.log(res);
+            promptPurchase();
+
+        })
+        })
+
+
+    });
+}
+
+// function findProduct(){
+
+// }
 
 
 
--- bamazonDB.js
+++ bamazonDB.js
@@ -1,23 +1,23 @@
-var mysql = require("mysql");
+// var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
+// var connection = mysql.createConnection({
+//     host: "localhost",
 
-    PORT: 3306,
+//     PORT: 3306,
 
-    user: "root",
-    password: "",
-    database: "bamazon_db"
-});
+//     user: "root",
+//     password: "35l4eJazz",
+//     database: "bamazon_db"
+// });
 
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
-    createProduct();
-  });
+// connection.connect(function(err) {
+//     if (err) throw err;
+//     console.log("connected as id " + connection.threadId + "\n");
+//     createProduct();
+//   });
 
-  app.listen(PORT, function() {
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
-  });
+//   app.listen(PORT, function() {
+//     // Log (server-side) when our server has started
+//     console.log("Server listening on: http://localhost:" + PORT);
+//   });
   
